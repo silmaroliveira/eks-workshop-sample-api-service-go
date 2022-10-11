582075c2233fabeae3b6a1373c57922804f6824a
silmaroliveira <115562742+silmaroliveira@users.noreply.github.com>
2022-10-11 10:56:15 -0300

    Change the message text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Ola Mundo!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
